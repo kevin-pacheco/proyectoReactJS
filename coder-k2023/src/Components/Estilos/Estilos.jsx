958b0723454c40ecf2a24a0355fccae0908129a5
kevin-pacheco <kevin.pacheco1210@gmail.com>
2023-02-24 13:18:12 -0300

    colorThemes-added

---
+++ coder-k2023/src/Components/Estilos/Estilos.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export const baseTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#7cb342",
+      light: "#96c267",
+    },
+    secondary: {
+      main: "#f4ff81",
+      light: "#ffffa3",
+      dark: "#b2b262",
+    },
+  },
+});
