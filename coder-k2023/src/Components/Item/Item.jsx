d715a48efc86a77223fb4170ef2fbbb231297a1c
kevin-pacheco <kevin.pacheco1210@gmail.com>
2023-03-10 18:10:11 -0300

    ok

--- coder-k2023/src/Components/Item/Item.jsx
+++ coder-k2023/src/Components/Item/Item.jsx
@@ -1,46 +1,59 @@
-import { Card, CardBody, CardFooter } from "@chakra-ui/react";
 import {
+  Card,
+  CardBody,
+  CardFooter,
   ButtonGroup,
-  Button,
   Image,
   Stack,
   Heading,
   Text,
   Divider,
 } from "@chakra-ui/react";
+import { ThemeProvider, Button } from "@mui/material";
+import { useState } from "react";
+import { baseTheme } from "../Estilos/Estilos";
 
 const Item = ({ element }) => {
+  const [theme] = useState(baseTheme);
+
   return (
-    <Card maxW="400">
-      <CardBody>
+    <Card>
+      <CardBody boxSize="350">
         <Image
-          boxSize="300"
+          maxHeight="250"
           src={element.img}
-          alt="Green double couch with wooden legs"
+          alt="imgProduct"
           borderRadius="lg"
         />
         <Stack mt="6" spacing="3">
           <Heading size="md">{element.title}</Heading>
           <Text>{element.description}</Text>
-          <Text color="primary.main" fontSize="2xl">
-            {element.price}
-          </Text>
+          <ThemeProvider theme={theme}>
+            <Text color="primary" fontSize="2xl">
+              ${element.price}
+            </Text>
+          </ThemeProvider>
         </Stack>
       </CardBody>
       <Divider />
-      <CardFooter>
-        <ButtonGroup spacing="2">
-          {/* <Link to={`/itemDetail/${element.id}`}>
+      {/* <Link to={`/itemDetail/${element.id}`}>
           <Button size="small" variant="contained" color="primary">
             Ver detalle
           </Button>
         </Link> */}
-          <Button variant="solid" colorScheme="green">
-            Details
-          </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Add to cart
-          </Button>
+      <CardFooter>
+        <ButtonGroup spacing="6.0rem">
+          <ThemeProvider theme={theme}>
+            <Button variant="contained" color="primary">
+              <Text color="white" fontSize="2xl">
+                Details
+              </Text>
+            </Button>
+
+            <Button variant="outlined" colorscheme="primary">
+              Add to cart
+            </Button>
+          </ThemeProvider>
         </ButtonGroup>
       </CardFooter>
     </Card>
